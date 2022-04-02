b4ae2c3e5572a6305d577a93b0552f8fee7b776d
변찬우 <chan00106@naver.com>
2022-04-02 23:54:35 +0900

    Change rocket

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,11 @@
-#![allow(non_snake_case)]
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+#[macro_use] extern crate rocket;
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("<h1>Hello world!</h1>")
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-#[actix_web::main]  
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
