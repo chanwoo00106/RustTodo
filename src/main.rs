d716537a1e109c4f33778926a2451b4586a04a80
변찬우 <byun00106@gmail.com>
2024-04-04 22:30:08 +0900

    feat: project reset

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-#[macro_use] extern crate rocket;
+use std::collections::HashMap;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+struct Todo {
+    map: HashMap<String, bool>,
 }
 
-#[get("/<name>")]
-fn hello(name: &str) -> String {
-    format!("Hello {}", name)
+impl Todo {
+    fn insert(&mut self, key: String) {
+        self.map.insert(key, false);
+    }
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, hello])
+fn main() {
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+
+    todo.insert(String::from("놀기"));
+
+    for i in todo.map {
+        println!("{:?}", i);
+    }
 }
