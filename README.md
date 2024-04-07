a5cd9b8bece69cb81960cb58bee8e74958123205
변찬우 <byun00106@gmail.com>
2024-04-07 22:44:48 +0900

    feat: add backend

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust Todo
+
+Svelt + Rust
+
+[Youtube Linke](https://youtu.be/w7is2bCTUg0?si=D-JY-RNVIcCadr_b)
---
+++ backend/src/main.rs
@@ -0,0 +1,12 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(index));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() -> String {
+    String::from("Hello World")
+}
