dd2e2cbe4339b0fb55c5dc36a06a1657ba9ae2ed
변찬우 <byun00106@gmail.com>
2024-04-10 00:48:09 +0900

    refactor: update config parse code

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -2,15 +2,30 @@ use std::{env, fs::File, io::Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("Not found file");
+    let mut f = File::open(config.filename).expect("Not found file");
 
     let mut content = String::new();
     f.read_to_string(&mut content)
         .expect("Something went wrong reading the file");
 
     println!("{}", content);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
