ff046693dd08801282732bf691839302be701a0c
변찬우 <byun00106@gmail.com>
2024-04-10 22:29:36 +0900

    refactor: add error handling

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,8 +1,11 @@
-use std::{env, fs::File, io::Read};
+use std::{env, fs::File, io::Read, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -22,10 +25,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
