6425d3ce1124b5ba09b1f0e13158e182448a266a
변찬우 <byun00106@gmail.com>
2024-04-12 11:23:16 +0900

    feat: add search function

--- greprs/src/lib.rs
+++ greprs/src/lib.rs
@@ -28,3 +28,32 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if !line.contains(query) {
+            continue;
+        }
+
+        result.push(line);
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
