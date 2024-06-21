61cfddc3247bacd8fedba67d8a8d174c5bcd6c99
변찬우 <byun00106@gmail.com>
2024-06-21 18:17:22 +0900

    feat: finish mars weight program

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,5 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Weight on Mars: {}kg", calculate_weight_on_mars(100.0));
+    let mut input = String::new();
+    println!("Enter your weight (kg): ");
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight = input.trim().parse::<f32>().unwrap();
+
+    println!("Weight on Mars: {}kg", calculate_weight_on_mars(weight));
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
