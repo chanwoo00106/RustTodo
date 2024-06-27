d55cb7e3c927288808a99c793581308d051411a4
변찬우 <byun00106@gmail.com>
2024-06-27 17:55:18 +0900

    feat: add modular

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,36 +1,10 @@
+use http::{Method, Request};
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
     let server = Server::new("localhost:8080".to_string());
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Server { addr }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
 }
