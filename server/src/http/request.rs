7d83fae2c006a12ba1074201a30259efb617cdb6
변찬우 <byun00106@gmail.com>
2024-07-03 15:59:36 +0900

    feat: add custom error

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,5 +1,6 @@
 use super::method::Method;
-use std::convert::TryFrom;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::{convert::TryFrom, error::Error};
 
 pub struct Request {
     path: String,
@@ -12,9 +13,41 @@ impl Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!()
     }
 }
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "InvalidRequest",
+            Self::InvalidEncoding => "InvalidEncoding",
+            Self::InvalidMethod => "InvalidMethod",
+            Self::InvalidProtocol => "InvalidProtocol",
+        }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
