ebfe736c6db8ab674933db352439d797a0c3e8c2
변찬우 <byun00106@gmail.com>
2024-07-28 23:07:15 +0900

    feat: add parse query string

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -25,6 +25,12 @@ impl TryFrom<&[u8]> for Request {
 
         let method: Method = method.parse()?;
 
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
+        }
+
         unimplemented!();
     }
 }
