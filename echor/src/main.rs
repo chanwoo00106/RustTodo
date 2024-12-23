eec642fe0d2676d4acae7329538ca77b793b5180
chanwoo00106 <byun00106@gmail.com>
2024-12-23 17:13:06 +0900

    refactor: change clap pattern

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,27 +1,20 @@
-use clap::{Arg, ArgAction, Command};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
 
 fn main() {
-    let matches = Command::new("echor")
-        .version("0.1.0")
-        .author("chan <chan00106@naver.com>")
-        .about("Rust version of echo")
-        .arg(
-            Arg::new("text")
-                .value_name("TEXT")
-                .help("Input text")
-                .required(true)
-                .num_args(1..),
-        )
-        .arg(
-            Arg::new("omit_newline")
-                .short('n')
-                .action(ArgAction::SetTrue)
-                .help("Do not print newline"),
-        )
-        .get_matches();
-
-    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
-    let omit_newline = matches.get_flag("omit_newline");
-
-    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
+    let args = Args::parse();
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+    );
 }
