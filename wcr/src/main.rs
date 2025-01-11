c3bc22d68ecb11a087b17bff156136dc1c4119ef
chanwoo00106 <byun00106@gmail.com>
2025-01-11 23:23:37 +0900

    feat: complete wcr project

--- wcr/src/main.rs
+++ wcr/src/main.rs
@@ -38,6 +38,41 @@ struct FileInfo {
     chars_count: usize,
 }
 
+impl FileInfo {
+    fn new() -> Self {
+        Self {
+            lines_count: 0,
+            words_count: 0,
+            bytes_count: 0,
+            chars_count: 0,
+        }
+    }
+
+    fn add(self: &mut Self, file_info: &Self) {
+        self.lines_count += file_info.lines_count;
+        self.words_count += file_info.words_count;
+        self.bytes_count += file_info.bytes_count;
+        self.chars_count += file_info.chars_count;
+    }
+
+    fn print_info(self: &Self, filename: &str, args: &Args) {
+        if args.lines {
+            print!("{:>8}", self.lines_count);
+        }
+        if args.words {
+            print!("{:>8}", self.words_count);
+        }
+        if args.bytes {
+            print!("{:>8}", self.bytes_count);
+        }
+        if args.chars {
+            print!("{:>8}", self.chars_count);
+        }
+
+        println!(" {}", if filename == "-" { "" } else { filename });
+    }
+}
+
 fn open(filename: &str) -> Result<Box<dyn BufRead>> {
     match filename {
         "_" => Ok(Box::new(BufReader::new(io::stdin()))),
@@ -84,27 +119,23 @@ fn run(mut args: Args) -> Result<()> {
         args.bytes = true;
     }
 
-    for filename in args.files {
+    let mut total_info = FileInfo::new();
+
+    for filename in &args.files {
         match open(&filename) {
             Err(e) => eprintln!("{e}"),
             Ok(file) => {
                 let file_info = read_file_count(file)?;
 
-                if args.lines {
-                    print!("{:>8}", file_info.lines_count);
-                }
-                if args.words {
-                    print!("{:>8}", file_info.words_count);
-                }
-                if args.bytes {
-                    print!("{:>8}", file_info.bytes_count);
-                }
-                if args.chars {
-                    print!("{:>8}", file_info.chars_count);
-                }
-                println!(" {filename}");
+                file_info.print_info(filename, &args);
+
+                total_info.add(&file_info);
             }
         }
+    }
+
+    if args.files.len() > 1 {
+        total_info.print_info("total", &args);
     }
 
     Ok(())
