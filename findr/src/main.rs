30108c599f01a5c95820f089da4b0db074d88872
chanwoo00106 <byun00106@gmail.com>
2025-02-04 21:48:49 +0900

    feat: complete findr project

--- findr/src/main.rs
+++ findr/src/main.rs
@@ -1,8 +1,9 @@
 use std::process;
 
 use anyhow::Result;
-use clap::{builder::PossibleValue, Parser, ValueEnum};
+use clap::{builder::PossibleValue, ArgAction, Parser, ValueEnum};
 use regex::Regex;
+use walkdir::{DirEntry, WalkDir};
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 enum EntryType {
@@ -28,22 +29,79 @@ impl ValueEnum for EntryType {
 #[derive(Debug, Parser)]
 #[command(version, about)]
 struct Args {
-    #[arg(default_value = ".", num_args = 1.., help = "Search paths")]
+    #[arg(
+        default_value = ".",
+        num_args = 0..,
+        help = "Search paths",
+        value_name = "PATH"
+    )]
     paths: Vec<String>,
-    #[arg(short = 'n', long = "name", num_args = 1.., value_name = "NAME", help = "Name")]
+
+    #[arg(
+        short = 'n',
+        long = "name",
+        num_args = 0..,
+        value_name = "NAME",
+        help = "Name",
+        action = ArgAction::Append,
+        value_parser = Regex::new
+    )]
     names: Vec<Regex>,
-    #[arg(short = 't', long = "type", num_args = 1..3, value_name = "TYPE", help = "Entry type")]
+
+    #[arg(
+        short = 't',
+        long = "type",
+        num_args = 0..,
+        value_name = "TYPE",
+        help = "Entry type",
+        action = ArgAction::Append,
+        value_parser = clap::value_parser!(EntryType)
+    )]
     entry_types: Vec<EntryType>,
 }
 
 fn run(args: Args) -> Result<()> {
-    println!("{:?}", args);
+    let type_filter = |entry: &DirEntry| {
+        args.entry_types.is_empty()
+            || args.entry_types.iter().any(|entry_type| match entry_type {
+                EntryType::Dir => entry.file_type().is_dir(),
+                EntryType::File => entry.file_type().is_file(),
+                EntryType::Link => entry.file_type().is_symlink(),
+            })
+    };
+
+    let name_filter = |entry: &DirEntry| {
+        args.names.is_empty()
+            || args
+                .names
+                .iter()
+                .any(|name| name.is_match(&entry.file_name().to_string_lossy()))
+    };
+
+    for path in args.paths {
+        let entries = WalkDir::new(path)
+            .into_iter()
+            .filter_map(|e| match e {
+                Err(e) => {
+                    eprintln!("{e}");
+                    Option::None
+                }
+                Ok(entry) => Some(entry),
+            })
+            .filter(type_filter)
+            .filter(name_filter)
+            .map(|entry| entry.path().display().to_string())
+            .collect::<Vec<_>>();
+
+        println!("{}", entries.join("\n"));
+    }
+
     Ok(())
 }
 
 fn main() {
     if let Err(e) = run(Args::parse()) {
-        eprint!("{e}");
+        eprintln!("{e}");
         process::exit(1);
     }
 }
